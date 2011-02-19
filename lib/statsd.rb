0bc7293f848fea10ba8c86e7973f48ea9f63a015
Rein Henrichs <rein@phpfog.com>
2011-02-18 18:39:56 -0800

    Adding yard documentation

--- lib/statsd.rb
+++ lib/statsd.rb
@@ -1,15 +1,38 @@
 require 'socket'
 
+# = Statsd: A Statsd client (https://github.com/etsy/statsd)
+#
+# Example:
+#
+#     statsd = Statsd.new 'localhost', 8125
+#
+#     statsd.increment 'garets'
+#     statsd.timing 'glork', 320
 class Statsd
   attr_reader :host, :port
+
+  # @param [String] host your statsd host
+  # @param [Integer] port your statsd port
   def initialize(host, port)
     @host, @port = host, port
   end
 
+  # @param [String] stat stat name
+  # @param [Integer] sample_rate sample rate, 1 for always
   def increment(stat, sample_rate=1);     count stat, 1,  sample_rate end
+
+  # @param [String] stat stat name
+  # @param [Integer] sample_rate sample rate, 1 for always
   def decrement(stat, sample_rate=1);     count stat, -1, sample_rate end
 
+  # @param [String] stat stat name
+  # @param [Integer] count count
+  # @param [Integer] sample_rate sample rate, 1 for always
   def count( stat, count, sample_rate=1); send stat, count, 'c',  sample_rate end
+
+  # @param [String] stat stat name
+  # @param [Integer] ms timing in milliseconds
+  # @param [Integer] sample_rate sample rate, 1 for always
   def timing(stat, ms,    sample_rate=1); send stat, ms,    'ms', sample_rate end
 
   def sampled(sample_rate)
