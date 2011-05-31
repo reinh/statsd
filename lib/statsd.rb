411d15947d30fb1fd7ccba4c54494f8012407271
Ray Krueger <raykrueger@gmail.com>
2011-05-31 11:14:30 -0500

    Replace :: with . as a delimiter, also replace reserved chars [:|@] with _
    This allows for ruby class names to be used as stat names.
    Also cleaning up stat names to remove statsd reserved characters

--- lib/statsd.rb
+++ lib/statsd.rb
@@ -9,6 +9,8 @@ require 'socket'
 #     statsd.increment 'garets'
 #     statsd.timing 'glork', 320
 class Statsd
+
+  RESERVED_CHARS_REGEX = /[\:\|\@]/
   attr_accessor :namespace
   
   # @param [String] host your statsd host
@@ -50,6 +52,7 @@ class Statsd
 
   def send(stat, delta, type, sample_rate)
     prefix = "#{@namespace}." unless @namespace.nil?
+    stat = stat.gsub('::', '.').gsub(RESERVED_CHARS_REGEX, '_')
     sampled(sample_rate) { socket.send("#{prefix}#{stat}:#{delta}|#{type}#{'|@' << sample_rate.to_s if sample_rate < 1}", 0, @host, @port) }
   end
 
