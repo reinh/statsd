ec9f416a0355d8341cc881284876c3f9e83fc803
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-07-16 17:16:52 +0100

    Fix TCP support
    Newlines are required.

--- lib/statsd.rb
+++ lib/statsd.rb
@@ -96,7 +96,7 @@ class Statsd
       # one, so the post-queue check will always catch it
       if (@batch_byte_size && @backlog_bytesize + message.bytesize + 1 > @batch_byte_size) ||
           (@flush_interval && last_flush_seconds_ago >= @flush_interval)
-        flush 
+        flush
       end
       @backlog << message
       @backlog_bytesize += message.bytesize
@@ -476,8 +476,8 @@ class Statsd
       # send(2) is atomic, however, in stream cases (TCP) the socket is left
       # in an inconsistent state if a partial message is written. If that case
       # occurs, the socket is closed down and we retry on a new socket.
+      message = @protocol == :tcp ? message + "\n" : message
       n = socket.write(message)
-
       if n == message.length
         break
       end
